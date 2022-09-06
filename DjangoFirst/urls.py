951bf202c345eeb1cb4a7a4cc311d25800355178
FanJekkk <47693218+FanJekkk@users.noreply.github.com>
2022-09-06 21:31:36 +0300

    first commit

--- DjangoFirst/urls.py
+++ DjangoFirst/urls.py
@@ -20,4 +20,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.home),
     path('about', views.about),
+    path('item/<int:id>', views.item_page),
+    path('items', views.items_list)
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render, HttpResponse
 
-name = "Иван"
-surname = "Иванов"
+name = "Денис"
+surname = "Тепляков"
+email = "denteplyakov2020@gmail.com"
+items = [
+   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+   {"id": 3, "name": "Coca-cola 1 литр" ,"quantity":12},
+   {"id": 4, "name": "Картофель фри" ,"quantity":0},
+   {"id": 5, "name": "Кепка" ,"quantity":124},
+]
+
+def items_list(request):
+    result = "<ol>"
+    for i in items:
+        result +=f"<li><a href='/item/{i['id']}'>{i['name']}</a></li>"
+    result += "</ol>"
+    return HttpResponse(result)
 
 def about(request):
-    info = f"<h1><i>Автор: {name} {surname}</i></h1>"
+    info = f"""Имя: <strong>{name}</strong><br>
+             Фамилия: <strong>{surname}</strong><br>
+             телефон: <strong>8-923-600-01-02</strong><br>
+             email: <strong>{email}</strong>"""
     return HttpResponse(info)
 
 def home(request):
-    return HttpResponse("Hello")
+    text = f"""<h1> "Изучаем django" </h1>
+    <strong> Автор </strong>: <i> {surname}
+    {name[0]}. </i>"""
+    return HttpResponse(text)
+
+def item_page(request,id):
+    for item in items:
+        if item["id"] == id:
+            item_str = f"Товар: <b>{item['name']}</b> количество: {item['quantity']}"
+            return HttpResponse(item_str)
+    return HttpResponse(f"Товар с id={id} не найден")
+
